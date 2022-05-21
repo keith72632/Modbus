2a62fad2810c44287d91769d90c5b6a4821c0c36
keith72632 <keithhorton30@outlook.com>
2022-05-21 18:34:12 -0500

    added client and server

---
+++ client.py
@@ -0,0 +1,13 @@
+from pyModbusTCP.client import ModbusClient
+
+ADDR = "192.168.0.188"
+PORT = 12345
+
+def main():
+    client = ModbusClient(ADDR, PORT)
+    client.open()
+    input = client.read_holding_registers(0)
+    print(f"Register value {input}")
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,43 @@
+from pyModbusTCP.server import ModbusServer, DataBank
+from time import sleep
+from random import uniform 
+
+ADDR = "192.168.0.188"
+PORT = 12345
+
+def main():
+    server = ModbusServer(ADDR, PORT, no_block=True)
+
+    try:
+        print(f"Starting server on address {ADDR} on port {PORT}")
+        server.start()
+        print("server online...")
+        state = [0]
+        while True:
+            DataBank.set_words(0, [int(uniform(0, 100))])
+            if state != DataBank.get_words(1):
+                state = DataBank.get_words(1)
+                print(f"Value of register 1 {state}")
+                sleep(0.5)
+    except KeyboardInterrupt:
+        print("\nkeyboard interrupt")
+        server.stop()
+        print("server stopped...")
+    except:
+        print("server shutdown...")
+        server.stop()
+        print("server is offline")
+if __name__ == "__main__":
+    main()
+
+"""
+Modbus Function Codes
+    Read Coils               = 0x01
+    Read Discrete Inputs     = 0x02
+    Read Holding Registers   = 0x03
+    Read Input Registers     = 0x04
+    Write Single Coil        = 0x05
+    Write Single Register    = 0x06
+    Write Multiple Coils     = 0x0F
+    Write Multiple Registers = 0x10
+"""
