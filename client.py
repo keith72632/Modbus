8c6d2a30c7407c45ff014681d0d028e0593481b1
keith72632 <keithhorton30@outlook.com>
2022-05-23 18:09:53 -0500

    client

--- client.py
+++ client.py
@@ -3,13 +3,20 @@ from pyModbusTCP.client import ModbusClient
 ADDR = "192.168.0.188"
 PORT = 5502
 
-def main(addr, port):
+def main(addr, port, op_code):
     client = ModbusClient(addr, port)
     client.open()
-    input = client.read_discrete_inputs(0, 100)
-    print(f"Register value {input}")
+    if op_code == 1:
+        coils = client.read_discrete_inputs(0, 100)
+        print(coils)
+    elif op_code == 4:
+        holding = client.read_holding_registers(101, 100)
+        print(holding)
+    else:
+        print("Invalide opcode")
 
 if __name__ == "__main__":
     addr = input("Enter address of modbus master:\n")
     port = input("Enter port:\n")
-    main(addr, port)
+    op_code = input("Enter op code:\n\t1. Read Coils\n\t4.Read Holding Register\n\t->")
+    main(addr, port, int(op_code))
