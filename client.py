2229038175e7235439963af49a4f4440bd6bdffb
keith72632 <keithhorton30@outlook.com>
2022-05-23 17:55:54 -0500

    added to client

--- client.py
+++ client.py
@@ -3,11 +3,13 @@ from pyModbusTCP.client import ModbusClient
 ADDR = "192.168.0.188"
 PORT = 5502
 
-def main():
-    client = ModbusClient(ADDR, PORT)
+def main(addr, port):
+    client = ModbusClient(addr, port)
     client.open()
     input = client.read_discrete_inputs(0, 100)
     print(f"Register value {input}")
 
 if __name__ == "__main__":
-    main()
+    addr = input("Enter address of modbus master:\n")
+    port = input("Enter port:\n")
+    main(addr, port)
